8db6f0da999d8da9e0e96a06b24241363a9da185
filet-mignon <mignon.ucla@gmail.com>
2015-04-23 20:54:00 -0700

    Simple Command Implementation
    Added implementation for executing simple commands.
    Also added some references on how to use the <unistd.h> library in
    Notes.txt

--- Lab-1/execute-command.c
+++ Lab-1/execute-command.c
@@ -23,5 +23,36 @@ execute_command (command_t c, bool time_travel)
 	//We can then use a recursive call to execute_command to execute other commands.
 
 
+	switch( c->type ){
+	case SIMPLE_COMMAND:
+		
+		//If there is an input redirection
+		if( input != 0 )
+		{
+			int fd = open( input, O_RDONLY, 0444 );
+			if ( fd < 0 )
+			{
+				//Is trying to read from a file that doesn't exist
+				return;
+			}
+
+			dup2(fd, 0); //Changes the standard input to read from this file.
+		}
+		//If there is an output redirection
+		if( output != 0 )
+		{
+			int fd = open( output, O_CREAT|O_TRUNC|O_WRONLY, 0644 );
+			if ( fd < 0 ) 
+				return;
+			dup2(fd, 1); //redirects the output to the file specified
+		}
+
+		execvp( word[0], word );
+
+		
+	default:
+
+	}
+
   error (1, 0, "command execution not yet implemented");
 }
