f4dcf13a7510de5016475b1aa5a7c44611c93979
filet-mignon <mignon.ucla@gmail.com>
2015-04-24 23:31:36 -0700

    First submission
    Added PIPE_COMMAND and updated README.

--- Lab-1/execute-command.c
+++ Lab-1/execute-command.c
@@ -10,7 +10,11 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
-#include <fcnt1.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+
 int
 command_status (command_t c)
 {
@@ -24,8 +28,11 @@ execute_command (command_t c, bool time_travel)
 	//We can then use a recursive call to execute_command to execute other commands.
 	//Should execute the command and then set the process's status so that it can return
 	//the correct value.
-        pid_t child;
-        int child_status; 
+    pid_t child;
+    int child_status; 
+	int fd[2];
+	const int READ = 0;
+	const int WRITE = 1;
 	
 	switch( c->type ){
 	case SIMPLE_COMMAND:
@@ -117,10 +124,62 @@ execute_command (command_t c, bool time_travel)
 		c->status = c->u.subshell_command->status;
 		break;
 	case PIPE_COMMAND:
+		if(pipe(fd) < 0)
+			error(1, 0, "Error creating pipe\n");
+		int firstpid = fork();
+
+		if (firstpid == 0)
+		{
+			//Child process will execute right command
+			close(fd[WRITE]);
+			if(dup2(fd[READ], READ) < 0)
+				error(1, 0, "Error redirecting read end\n");
+			execute_command( c->u.command[1], time_travel );
+			c->status = c->u.command[1]->status;
+			close(fd[READ]);
+		}
+
+		else if (firstpid > 0)
+		{
+			int secondpid = fork();
+			if(secondpid == 0)
+			{
+				//Second child process will execute left command.
+				close(fd[0]);
+				if(dup2(fd[WRITE], WRITE) < 0)
+					error(1, 0, "Error redirecting write end\n");
+				execute_command( c->u.command[0], time_travel );
+				c->status = c->u.command[0]->status;
+				close(fd[WRITE]);
+			}
+			else if( secondpid > 0)
+			{
+				close(fd[READ]);
+				close(fd[WRITE]);
+				int status;
+				int returnedpid = waitpid(-1, &status, 0);
+				//first process finished first
+				if(returnedpid == secondpid)
+					waitpid(firstpid, &status, 0);
+
+				//second process finished first
+				if(returnedpid == firstpid)
+					waitpid(secondpid, &status, 0);
+			}
+
+		}
+		else
+		{
+			error(1, 0, "Error creating first child process\n");
+		}
+
 		break;
+
 	default:
+
 		//NOT A VALID COMMAND TYPE
+		  error (1, 0, "Command type invalid\n");
+		  break;
 	}
 
-  error (1, 0, "command execution not yet implemented");
 }
