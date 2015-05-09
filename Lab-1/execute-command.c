b7e743f9d77ac0b229f35682e818bcd4f390dbe8
David Thomas Scheibe <scheibe@lnxsrv01.seas.ucla.edu>
2015-05-09 10:11:43 -0700

    Finished execute-command.c for lab1c - Now to begin testing

--- Lab-1/execute-command.c
+++ Lab-1/execute-command.c
@@ -14,6 +14,266 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
+#define BUFFER_SIZE 100
+
+/*      ----------- Structure for Queue --------------  */
+
+typedef struct{
+  struct GraphNode* front;
+  struct GraphNode* end;
+} Queue;
+
+bool QueueIsEmpty(Queue* q)
+{
+  return (q->front == NULL);
+}
+
+void Queue_Insert(Queue* q, command_t command)
+{
+  //Make a new command_list node first
+  struct GraphNode* toAdd = (GraphNode*)checked_malloc(sizeof(GraphNode));
+  toAdd->command = command;
+  toAdd->pid = -1;
+  toAdd->next = NULL;
+
+  if(QueueIsEmpty(q))
+    {
+      q->front = toAdd;
+      q->end = toAdd;
+    }
+  q->end->next = toAdd;
+  q->end = toAdd;
+}
+
+struct command_t Queue_Next(Queue* q)
+{
+  command_t next;
+  GraphNode* toRemove;
+  if(QueueIsEmpty(q))
+    return NULL;
+  else if(q->front == q->end) //Last node left
+    {
+      toRemove = q->front;
+      q->front = NULL;
+      q->end = NULL;
+    }
+  else
+    {
+      toRemove = q->front;
+      q->front = q->front->next;
+    }
+
+  next = toRemove->command;
+  free(toRemove);
+
+  return result;
+}
+/*      ----------- Structure for Dependency_Graph --------------  */
+
+typedef struct{
+  Queue* no_dependencies;
+  Queue* dependencies;
+  int nodep_size;
+  int dep_size;
+  int nodep_buffer;
+  int dep_buffer;
+}Dependency_Graph;
+
+
+
+typdef struct{
+  command_t command;
+  struct GraphNode** before;       //Is an array of GraphNode pointers
+  int before_size;                 //The size of the array 'before'
+  pid_t pid;                       //Initialized to -1
+  int buffer;
+}GraphNode;
+
+void insertGraphNode(GraphNode gnode, GraphNode new_node)
+{
+  if(gn->before == NULL)
+    {
+      gn->before = (GraphNode *) malloc(sizeof(GraphNode) * gn->buffer);
+      if (gn->before == NULL)
+	{
+	  error(1,0,"malloc error");
+	  exit(-1);
+	}
+    }
+  else if (gn->before_size == gn->buffer)
+    {
+      gn->buffer = gn->buffer * 2;
+      gn->before = (GraphNode *) realloc(gn->before, sizeof(GraphNode) * gn->buffer);
+      if (gn->before == NULL)
+	{
+	  error(1,0,"Realloc error");
+	  exit(-1)
+	}
+    }
+  gn->before[gn->before_size++] = new_node;
+  return;
+}
+
+typedef struct{
+  ListNode next;
+  GraphNode g_node;
+  char **read_list;
+  char **write_list;
+  int read_buffer;
+  int write_buffer;
+  int rl_size;
+  int wl_size;
+}ListNode;
+
+void addToReadList(ListNode lnode, char *s)
+{
+  if (lnode->read_list == NULL)
+    {
+      lnode->read_list = (char **) malloc(sizeof(char *) * lnode->read_buffer);
+      if (lnode->read_list == NULL)
+	{
+	  error(1,0,"malloc error");
+	  exit(-1);
+	}
+    }
+  else if (lnode->rl_size == lnode->read_buffer)
+    {
+      lnode->read_buffer = lnode->read_buffer * 2;
+      ln->read_list = (char **) realloc(sizeof(char *) * lnode->read_buffer);
+      if (lnode->read_list == NULL)
+	{
+	  error(1,0,"malloc error");
+	  exit(-1);
+	}
+    }
+  lnode->read_list[ln->rl_size++] = s;
+  return;
+}
+
+void addToWriteList(ListNode lnode, char *s)
+{
+  if (lnode->write_list == NULL)
+    {
+      lnode->write_list =(char **) malloc(sizeof(char *) * lnode->write_buffer);
+      if (lnode->write_list == NULL)
+	{
+	  error(1,0,"malloc error");
+	  exit(-1);
+	}
+    }
+  else if (lnode->wl_size == lnode->write_buffer)
+    {
+      lnode->write_buffer = lnode->write_buffer *2;
+      ln->write_list = (char **) realloc(sizeof(char *) * lnode->write_buffer);
+      if (lnode->write_list == NULL)
+	{
+	  error(1,0,"malloc error");
+	  exit(-1);
+	}
+    }
+  lnode->write_list[ln->wl_size++] = s;
+  return;
+}
+
+void processCommand(command_t command, ListNode lnode)
+{
+  if (command->type == SIMPLE_COMMAND)
+    {
+      for (int i = 1; command->u.word[i] != NULL; i++)
+	{
+	  if (command->u.word[i][0] != '-')
+	    {
+	      addToReadList(lnode, command->u.word[i]);
+	    }
+	}
+      if (command->output != NULL)
+	addToWriteList(lnode, command->output);
+      if (command->input != NULL)
+	addToReadList(lnode, command->input);
+    }
+  else if (command->type == SUBSHELL_COMMAND)
+    {
+      if (command->output != NULL)
+	addToWriteList(lnode, command->output);
+      if (command->input != NULL)
+	addToReadList(lnode, command->input);
+    }
+  else
+    {
+      processCommand(c->u.command[0], lnode);
+      processCommand(c->u.command[1], lnode);
+    }
+  return;
+}
+
+bool isDependent(char **list_a, int list_a_size, char **list_b, int list_b_size)
+{
+  for (int i = 0; i < list_a_size; i++)
+    {
+      for (int j = 0; j < list_b_size; j++)
+	{
+	  if (strcmp(list_a[i], list_b[j]) == 0)
+	    return true;
+	}
+    }
+  return false;
+}
+
+Dependency_Graph createGraph(command_stream_t c_stream)
+{
+  Dependency_Graph d_graph = (Dependency_Graph) malloc(sizeof(Dependency_Graph));
+  d_graph->no_dependencies = NULL;
+  d_graph->dependencies = NULL;
+  d_graph->nodep_size = 0;
+  d_graph->dep_size = 0;
+  d_graph->nodep_buffer = BUFFER_SIZE;
+  d_graph->dep_buffer = BUFFER_SIZE;
+
+  command_t command;
+  ListNode list = NULL;
+  while ((c = read_command_stream(c_stream)))
+    {
+      GraphNode g_node = (GraphNode) malloc(sizeof(GraphNode));
+      g_node->command = command;
+      g_node->before = NULL;     
+      g_node->before_size = 0;
+      g_node->pid = -1;               
+      g_node->buffer = BUFFER_SIZE;
+      
+      ListNode l_node = (ListNode) malloc(sizeof(ListNode));
+      l_node->next = NULL;
+      l_node->g_node = g_node;
+      l_node->read_list = NULL;
+      l_node->write_list = NULL;
+      l_node->read_buffer = BUFFER_SIZE;
+      l_node->write_buffer = BUFFER_SIZE;
+      l_node->rl_size = 0;
+      l_node->wl_size = 0;
+
+      processCommand(command, l_node);
+
+      if (list != NULL)
+	l_node->next = list;
+
+      ListNode temp_list = l_node->next;
+      while (temp_list != NULL)
+	{
+	  if (l_node->read_list != NULL && temp_list->write_list != NULL && isDependent(l_node->read_list, l_node->rl_size, temp->write_list, temp->wl_size))
+	    insertGraphNode(l_node->g_node, temp_list->g_node);
+	  else if (l_node->write_list != NULL && temp_list->read_list != NULL && isDependent(l_node->write_list, l_node->wl_size, temp_list->read_list, temp_list->rl_size))
+	    insertGraphNode(l_node->g_node, temp_list->g_node);
+	  else if (l_node->write_list != NULL && temp_list->write_list != NULL && isDependent(l_node->write_list, l_node->wl_size, temp_list->write_list, temp_list->wl_size))
+	    insertGraphNode(l_node->g_node, temp_list->g_node);
+	  temp_list = temp_list->next;
+	}
+      if (l_node->g_node->before == NULL)
+	Queue_Insert(d_graph->no_dependencies, command);
+      else
+	Queue_Insert(d_graph->dependencies, command);
+      list = l_node;
+    }
+  return d_graph;
+}
 
 int
 command_status (command_t c)
